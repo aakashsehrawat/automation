8048e6cd7229fca6d01c1b11a3fc384f962a38f8
aakashsehrawat <sehrawataakash@gmail.com>
2016-05-05 17:28:30 +0530

    Added files via upload

---
+++ Myclass.java
@@ -0,0 +1,103 @@
+package mypackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+
+		String appUrl = "http://www.tripfactory.com/";
+		driver.get(appUrl);
+
+		// Select the package on homepage
+
+		driver.findElement(By.xpath(".//*[@id='cntRl1975']/div[1]/a/div[2]")).click();
+
+		// price on tf homepage
+
+		String initprice = driver.findElement(By.xpath(".//*[@id='startingPrice']/em")).getText();
+		System.out.println(" The initial price is " + initprice);
+
+		// click on book online
+
+		driver.findElement(By.className("pkgAvailURL")).click();
+		System.out.println("Clicking on book online");
+
+		String price = driver.findElement(By.xpath(".//*[@id='invCalMth1']/table/tbody/tr[3]/td[5]/div[2]/div[2]/span"))
+				.getText();
+		System.out.println(" The price is " + price);
+
+		// click on Continue
+
+		driver.findElement(By.id("stp1CntActCtr"));
+		driver.findElement(By.xpath("//div[2]/div/div[3]/section/section/section/form/div[1]/div[1]/div[2]/div[2]/a"))
+				.click();
+
+		System.out.println("clicking Continue");
+
+		// click on Calculate price
+
+		String order = driver
+				.findElement(By.xpath("//div[2]/div/div[3]/section/section/div[1]/div/div[1]/div[3]/ul/li[3]"))
+				.getAttribute("innerHTML");
+		System.out.println(order);
+
+		// selecting the dropdown
+
+		Select Adults = new Select(driver.findElement(By.name("adults1")));
+		Adults.selectByVisibleText("1");
+
+		driver.findElement(By.id("calculateButton")).click();
+		System.out.println("clicking calculate price");
+
+		try {
+			Thread.sleep(1000 * 10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		order = driver.findElement(By.xpath("//div[2]/div/div[3]/section/section/div[1]/div/div[1]/div[3]/ul/li[3]"))
+				.getAttribute("innerHTML");
+		System.out.println(order);
+
+		String summary = driver.findElement(By.xpath(".//*[@id='order_summary']")).getText();
+		System.out.println(" The order summary is " + summary);
+
+		driver.findElement(By.id("bookbtn")).click();
+		System.out.println("clicking on continue booking");
+
+		// selecting the drop down
+
+		Select Title = new Select(driver.findElement(By.id("paxTitle1")));
+		Title.selectByVisibleText("Mr");
+
+		// enter values in text fields
+
+		driver.findElement(By.id("paxFirstName1")).sendKeys("test");
+		driver.findElement(By.id("paxLastName1")).sendKeys("test");
+
+		driver.findElement(By.id("email")).sendKeys("aakash@tripfactory.com");
+		driver.findElement(By.id("promoCheckBx")).click();
+
+		driver.findElement(By.id("mobile")).sendKeys("999999999");
+
+		// click on Proceed for payment
+
+		driver.findElement(By.linkText("Proceed for payment")).click();
+
+		String finalfare = driver.findElement(By.xpath(".//*[@id='totalFare']")).getText();
+		System.out.println(" The final fare is " + finalfare);
+
+		// navigate back to tripfactory's homepage
+
+		driver.findElement(By.xpath("//div[1]/div[1]/a/img")).click();
+
+		// close the browser
+		System.out.println("Test scripted executed successfully.");
+	}
+}
